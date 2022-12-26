6444f240844ef4531464669788376e4688be9812
Ali Rabiee <92788225+rabieeali@users.noreply.github.com>
2022-12-26 16:19:00 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,159 @@
+import { useRoutes } from 'react-router-dom';
+import router from 'src/router';
+
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+
+import { CssBaseline } from '@mui/material';
+import ThemeProvider from './theme/ThemeProvider';
+
+
+import { ThemeProvider as TH, createTheme } from "@mui/material/styles"
+import rtlPlugin from "stylis-plugin-rtl"
+import { CacheProvider } from "@emotion/react"
+import createCache from "@emotion/cache"
+import { prefixer } from "stylis"
+
+function App() {
+  const content = useRoutes(router);
+
+
+  const theme = createTheme({
+    direction: "rtl",
+    colors: {
+      gradients: {
+        blue1: '',
+        blue2: '',
+        blue3: '',
+        blue4: '',
+        blue5: '',
+        orange1: '',
+        orange2: '',
+        orange3: '',
+        purple1: '',
+        purple3: '',
+        pink1: '',
+        pink2: '',
+        green1: '',
+        green2: '',
+        black1: '',
+        black2: ''
+      },
+      shadows: {
+        success: '',
+        error: '',
+        primary: '',
+        warning: '',
+        info: ''
+      },
+      alpha: {
+        white: {
+          5: '',
+          10: '',
+          30: '',
+          50: '',
+          70: '',
+          100: ''
+        },
+        trueWhite: {
+          5: '',
+          10: '',
+          30: '',
+          50: '',
+          70: '',
+          100: ''
+        },
+        black: {
+          5: '',
+          10: '',
+          30: '',
+          50: '',
+          70: '',
+          100: ''
+        }
+      },
+      secondary: {
+        lighter: '',
+        light: '',
+        main: '',
+        dark: ''
+      },
+      primary: {
+        lighter: '',
+        light: '',
+        main: '',
+        dark: ''
+      },
+      success: {
+        lighter: '',
+        light: '',
+        main: '',
+        dark: ''
+      },
+      warning: {
+        lighter: '',
+        light: '',
+        main: '',
+        dark: ''
+      },
+      error: {
+        lighter: '',
+        light: '',
+        main: '',
+        dark: ''
+      },
+      info: {
+        lighter: '',
+        light: '',
+        main: '',
+        dark: ''
+      }
+    },
+    general: {
+      reactFrameworkColor: undefined,
+      borderRadiusSm: '',
+      borderRadius: '',
+      borderRadiusLg: '',
+      borderRadiusXl: ''
+    },
+    sidebar: {
+      background: undefined,
+      boxShadow: undefined,
+      width: '',
+      textColor: undefined,
+      dividerBg: undefined,
+      menuItemColor: undefined,
+      menuItemColorActive: undefined,
+      menuItemBg: undefined,
+      menuItemBgActive: undefined,
+      menuItemIconColor: undefined,
+      menuItemIconColorActive: undefined,
+      menuItemHeadingColor: undefined
+    },
+    header: {
+      height: '',
+      background: undefined,
+      boxShadow: undefined,
+      textColor: undefined
+    }
+  });
+
+  const cacheRTL = createCache({
+    key: "muirtl",
+    stylisPlugins: [prefixer, rtlPlugin]
+  })
+
+  return (
+    <CacheProvider value={cacheRTL}>
+      <TH theme={theme}>
+        <ThemeProvider>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <CssBaseline />
+            {content}
+          </LocalizationProvider>
+        </ThemeProvider>
+      </TH>
+    </CacheProvider>
+  );
+}
+export default App;
